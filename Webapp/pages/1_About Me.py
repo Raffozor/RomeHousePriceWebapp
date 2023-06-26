16e194a359efc34abf81c8204f766633cc9b09d9
Raffaello Cesetti <raffaello92@yahoo.it>
2023-06-27 00:25:23 +0200

    various updates

--- Webapp/pages/1_About Me.py
+++ Webapp/pages/1_About Me.py
@@ -1,3 +1,39 @@
 import streamlit as st
+from PIL import Image
 
-st.header("test")
+image = Image.open('foto.jpeg')
+st.set_page_config(
+    page_title="Raffo's WebApp",
+    page_icon="🤖"
+)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image(image, width=300)
+
+with col3:
+    st.write(' ')
+st.header("Raffaello Cesetti")
+st.markdown("Hi! My name is Raffaello Cesetti and I am a CESMA Master's student specializing in Data Science. "
+            "During an internship experience in Lisbon, I discovered my passion for Data Science, working with Python "
+            "to help the business migrate from data analysis on Excel to Python.")
+st.markdown("During that time, I met some guys "
+            "studying Data Science and have had the goal of becoming a Data Scientist ever since. I am fascinated by "
+            "Python and its endless libraries, which have become my tool of choice for Data Science.")
+st.markdown("I appreciate the "
+            "flexibility, simplicity,and versatility of Python, which allow me to work on every phase of a Data Science"
+            " project. I can collect data through web scraping and get as far as putting machine learning models into "
+            "production, such as this web-app on Streamlit.")
+st.markdown("I decided to develop this project as a personal exercise "
+            "to improve my skills in all phases of the data process. In the section dedicated to the model, "
+            "I explain in detail the work done.")
+st.markdown("I look forward to tackling real Data Science projects, and I hope this "
+            "small project will demonstrate my dedication and willingness to challenge myself. Thank you for reading "
+            "this introduction about me!")
+
+st.markdown("**LinkedIn**: https://www.linkedin.com/in/raffaello-cesetti/")
+st.markdown("**GitHub**:   https://github.com/Raffozor/")
---
+++ Webapp/pages/2_About the Project.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(
+    page_title="Raffo's WebApp",
+    page_icon="🤖"
+)
+st.header("The project explained")
+st.markdown("The objective of this project was to apply the knowledge acquired during the Master CESMA by developing "
+            "a proof of concept that demonstrates the practical application of our learning.")
+st.markdown("To begin the project, I created a web crawler and scraper using Selenium and Beautiful Soup to extract "
+            "information on houses for sale from an Italian Real Estate website. To sped up the process, I utilized "
+            "two PCs in parallel and updated a cloud-based MySQL database on AWS.")
+st.markdown("During the initial phase, a total of 15.522 observations were collected. After performing data cleaning "
+            "and eliminating observations with errors, the resulting dataset comprised 14.874 observations. "
+            "Subsequently, the dataset was divided into an 80% training set and a 20% test set.")
+st.markdown("For categorical variables with numerous categories, mean encoding with standard deviation was employed, "
+            "while one-hot encoding was used for variables with a low number of categories. The XGBoost model was "
+            "selected for this proof of concept.")
+st.markdown("The model underwent fine-tuning using a validation set of 20%. After the fine-tuning process, the model "
+            "achieved a mean absolute percentage error of approximately 15% on the test set.")
+st.markdown(
+    'The final step of this project involved putting the model into "production" by integrating it into a Streamlit '
+    'web application. This can allow users to estimate the price of a house based on the selected variables.')
+house_coordinates = pd.read_csv('house_coordinates.csv')
+st.map(house_coordinates, zoom=10)
+st.markdown("Coordinates plot of all houses available")
